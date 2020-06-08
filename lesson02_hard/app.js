91f223f5302cb550f258dd71c654405d43b91d10
ValBomj <123itunes2002@gmail.com>
2020-06-08 21:01:23 +0300

    fix(app.js) added slice() method and changed the type of variables

--- lesson02_hard/app.js
+++ lesson02_hard/app.js
@@ -1,10 +1,26 @@
-let num = 266219;
-let numString = num.toString();
-let numArr = numString.split('');
-let multiplyRes = 1;
-for (let i = 0; i < numArr.length; i++) {
-  multiplyRes *= numArr[i];
-}
-console.log(multiplyRes);
-let multiplyResExp = (multiplyRes ** 3).toString();
-console.log(multiplyResExp[0] + multiplyResExp[1]);
+const num = 266219;
+
+// число в строку
+const numString = num.toString();
+
+// строку в массив 
+const arr = numString.split('');
+
+// перемножаем все цыфры числа
+const result = arr.reduce(function(mult, current) {
+  return mult * current;
+}, 1);
+
+// выводим результат перемножения
+console.log( result ); 
+
+// возводим результат в 3 степень
+const pow = result ** 3;
+
+// результат возведения в степень переводим в строку
+const powString = pow.toString();
+
+
+console.log(powString.slice(0, 2));
+
+
