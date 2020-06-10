a2cd87d3e1f3de84e0fa4fd1847192ab3c5ab6bd
ValBomj <123itunes2002@gmail.com>
2020-06-10 22:46:53 +0300

    created foo function which remove spaces and add ... at the and

---
+++ lesson04_hard/app.js
@@ -0,0 +1,15 @@
+'use strict';
+function foo (argument) {
+  if (typeof(argument) !== 'string') {
+    console.log('Не строка');
+  } else if (argument.length >= 30) {
+    console.log(argument.trim().slice(0, 30) + '...');
+  } else {
+    console.log(argument);
+  }
+
+  // Вариант через тернарные операторы
+  typeof(argument) !== 'string' ? console.log('Не строка') : console.log(argument.trim().slice(0, 30) + '...');
+}
+  
+  foo('Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, amet.');
