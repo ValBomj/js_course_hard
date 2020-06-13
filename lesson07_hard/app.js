1b7570ffa365aee3a69e16e765a7ccee9da1bbc9
ValBomj <123itunes2002@gmail.com>
2020-06-13 23:31:26 +0300

    fix(app.js) fixed getting day name from new Date

--- lesson07_hard/app.js
+++ lesson07_hard/app.js
@@ -3,43 +3,30 @@
 // Создаю массив с днями недели
 const week = ['ВоскресеньЕ', 'ПонедЕльник', 'ВтоРник', 'СреДа', 'ЧеТверг', 'ПятнИца', 'СубботА'];
 
-// Перевожу getDay() из числа в слово
-let currentDay;
-const now = new Date().getDay();
-if (now === 0) {
-  const currentDay = 'воскресенье';
-} else if (now === 1) {
-  currentDay = 'понедельник';
-} else if (now === 2) {
-  currentDay = 'вторник';
-} else if (now === 3) {
-  currentDay = 'среда';
-} else if (now === 4) {
-  currentDay = 'четверг';
-} else if (now === 5) {
-  currentDay = 'пятница';
-} else if (now === 6) {
-  currentDay = 'суббота';
-}
+const date = new Date();
+const day = date.toLocaleString('ru', {weekday: 'long'});
+
 
 // Перебираю элементы массива
 week.forEach(function(item, i, array) {
 
   // Создаю элемент с тегом Р и вписываю в него item (день недели из массива)
   const elem = document.createElement('p');
-  elem.innerHTML = item;
-  // Привожу item к нижнему регистру для более точной проверки 
-  const lowerElem = elem.innerHTML.toLowerCase();
+  elem.textContent = item;
 
-  // Проверяю item на: Сегодняшний день, выходной, и выходной если он сегодня
-  if (lowerElem === currentDay && lowerElem === 'суббота' || lowerElem === currentDay && lowerElem === 'воскресенье') {
-    elem.style.cssText = 'font-weight: 700; font-style: italic;';
-  } else if (lowerElem === currentDay) {
-    elem.style.fontWeight = 700;
+  // Привожу item к нижнему регистру для более точной проверки 
+  const lowerElem = elem.textContent.toLowerCase();
+
+  if (lowerElem === day) {
+    elem.classList.add('bold');
+
+    if (lowerElem === 'суббота' || lowerElem === 'воскресенье') {
+      elem.classList.add('italic');
+    }
   } else if (lowerElem === 'суббота' || lowerElem === 'воскресенье') {
-    elem.style.fontStyle = 'italic';
+    elem.classList.add('italic');
   }
 
   // Добавляю элемент на страницу
-  document.body.append(elem)
+  document.body.appendChild(elem);
 });
