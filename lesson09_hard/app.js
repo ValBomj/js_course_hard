cff9706826359e2529af766e9a9a06fb681f3b04
ValBomj <123itunes2002@gmail.com>
2020-06-19 14:22:40 +0300

    fix (app.js) fixed date output in body, changed function names

--- lesson09_hard/app.js
+++ lesson09_hard/app.js
@@ -1,29 +1,7 @@
 "use strict";
-let todayA, day, date, month, monthB, newMonth, year, hours, minutes, seconds;
-function timeReset() {
-  todayA = new Date(),
-  // День недели на русском
-  day = todayA.toLocaleString('ru', {weekday: 'long'}),
-  // Число от 1 до 31
-  date = todayA.getDate(),
-  // Месяц на русском
-  month = todayA.toLocaleString('ru', {month: 'long'}),
-  // Число от 1 до 12
-  monthB = todayA.getMonth() + 1,
-  // Окончание "я" у месяца
-  newMonth = month.slice(0, month.length - 1) + 'я',
-  // Год
-  year = todayA.getFullYear(),
-  // Часи
-  hours = todayA.getHours(),
-  // Минуты 
-  minutes = todayA.getMinutes(),
-  // Секунд
-  seconds = todayA.getSeconds()
-}
 
 // Склонение слова в зависимости от number (число)
-const univ = function (number, arr) {  
+const timeDeclination = function (number, arr) {  
   const options = [2, 0, 1, 1, 1, 2];  
   if (number % 100 >= 5 && number % 100 <= 19) {
     return number + ' ' + arr[2] + ' ';
@@ -35,24 +13,46 @@ const univ = function (number, arr) {
 }
 
 // Добавление 0 вначало если число меньше 10
-function univ2 (date) {
-  if (date > 0 && date <= 9) {
+function addZero (date) {
+  if (date >= 0 && date <= 9) {
     return '0' + date;
   } else {
     return date;
   }
 }
 
+const body = document.querySelector('body');
+const dateFormatA = document.createElement('p');
+const dateFormatB = document.createElement('p');
+body.appendChild(dateFormatA);
+body.appendChild(dateFormatB);
+
+const hour = ['час', 'часа', 'часов'];
+const minute = ['минута', 'минуты', 'минут'];
+const second = ['секунда', 'секунды', 'секунд'];
 
 const inter = setInterval(function() {
-  const html = document.querySelector('html');
-  const dateFormatA = document.createElement('p');
-  const dateFormatB = document.createElement('p');
+  const todayA = new Date();
+  // День недели на русском
+  const day = todayA.toLocaleString('ru', {weekday: 'long'});
+  // Число от 1 до 31
+  const date = todayA.getDate();
+  // Месяц на русском
+  const month = todayA.toLocaleString('ru', {month: 'long'});
+  // Число от 1 до 12
+  const monthB = todayA.getMonth() + 1;
+  // Окончание "я" у месяца
+  const newMonth = month.slice(0, month.length - 1) + 'я';
+  // Год
+  const year = todayA.getFullYear();
+  // Часы
+  const hours = todayA.getHours();
+  // Минуты 
+  const minutes = todayA.getMinutes();
+  // Секунды
+  const seconds = todayA.getSeconds();
 
-  timeReset();
-  dateFormatA.textContent = 'Сегодня ' + day + ', ' + date + ' ' +  newMonth + ' ' + year + ' года, ' + univ(hours, ['час', 'часа', 'часов' ]) + univ(minutes, ['минута', 'минуты', 'минут']) + univ(seconds, ['секунда', 'секунды', 'секунд']);
-  dateFormatB.textContent = univ2(date) + '.' + univ2(monthB) + '.' + univ2(year) + ' - ' + univ2(hours) + ':' + univ2(minutes) + ':' + univ2(seconds);
+  dateFormatA.textContent = 'Сегодня ' + day + ', ' + date + ' ' +  newMonth + ' ' + year + ' года, ' + timeDeclination(hours, hour) + timeDeclination(minutes, minute) + timeDeclination(seconds, second);
+  dateFormatB.textContent = addZero(date) + '.' + addZero(monthB) + '.' + addZero(year) + ' - ' + addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds);
 
-  html.appendChild(dateFormatA);
-  html.appendChild(dateFormatB);
 }, 1000);
